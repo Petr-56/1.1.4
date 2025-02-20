37c3ee166a1eda1f6c1b2f8ba770028389f19bb0
Petr-56 <petkog2156-2@mail.ru>
2025-02-20 16:15:37 +0300

    Update Util.java

--- PP_1_1_3-4_JDBC_Hibernate-master(2)/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_1_1_3-4_JDBC_Hibernate-master(2)/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,10 +3,38 @@ package jm.task.core.jdbc.util;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
 
 public class Util {
-    private static Configuration configuration = new Configuration().addAnnotatedClass(User.class);
-    public static SessionFactory getSessionFactory() {return configuration.buildSessionFactory(); }
+    private static SessionFactory sessionFactory;
 
+    static {
+        try {
+            Configuration configuration = new Configuration();
 
+            configuration.addAnnotatedClass(User.class);
+
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mydbtest");
+            settings.put(Environment.USER, "root");
+            settings.put(Environment.PASS, "228228228");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            settings.put(Environment.HBM2DDL_AUTO, "update");
+
+            configuration.setProperties(settings);
+
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
